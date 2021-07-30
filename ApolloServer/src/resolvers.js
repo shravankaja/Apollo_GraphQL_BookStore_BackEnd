3d562ca133e749133b2bb37f12c8b253b4ccfb5a
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-07-30 15:23:19 +0530

    Added: resolvers for graphql

---
+++ ApolloServer/src/resolvers.js
@@ -0,0 +1,10 @@
+const {signUp,signIn} = require('../Mutations/Users/users')
+
+
+exports.resolvers = {
+        Mutation : {
+                 signUp,
+                                      signIn
+
+        }
+}

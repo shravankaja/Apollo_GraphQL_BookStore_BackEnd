7b0ef88093d0b7a2787f95dc5625778254e1239a
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-08-02 14:55:25 +0530

    Added: package-lock file to define the tree structure for node modules

--- testhusky.js
+++ testhusky.js
@@ -1,6 +1,5 @@
-let i =0    
+let i = 0;
 
-
-                        for(let i=0 ; i < 10 ; i++) {
-                            console.log(i)
-                        }
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}

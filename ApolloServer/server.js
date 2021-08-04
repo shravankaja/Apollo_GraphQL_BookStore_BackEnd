cc77ba17f48b27e78fffc871d774928b2a378d81
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-08-04 14:27:16 +0530

    Refactored: server.js file to synchronize db connection after server connection

--- ApolloServer/server.js
+++ ApolloServer/server.js
@@ -1,9 +1,19 @@
 const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("../ApolloServer/src/schema");
 const { resolvers } = require("../ApolloServer/src/resolvers");
+let mongoose = require("mongoose");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen(4000).then(({ url }) => {
+server.listen(4001).then(({ url }) => {
   console.log(`Server ready at ${url}`);
+
+  mongoose
+    .connect("mongodb://localhost:27017/BookStore")
+    .then(() => {
+      console.log("connection successfull yes");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });

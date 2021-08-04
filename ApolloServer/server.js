ca5c803f071ca3823d7ac06b9dc9f4ff7d918e21
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-08-04 14:04:26 +0530

    Delete server.js

--- ApolloServer/server.js
+++
@@ -1,9 +0,0 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("../ApolloServer/src/schema");
-const { resolvers } = require("../ApolloServer/src/resolvers");
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen(4000).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
